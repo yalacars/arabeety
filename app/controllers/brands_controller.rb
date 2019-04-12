733b22d9775b6713950faaf6a998f8eac2290f79
Riyad Anabtawi <riyad.faek@gmail.com>
2019-04-12 16:47:58 -0500

    fixed order and fixed adding tiers years

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,7 +4,7 @@ class BrandsController < ApplicationController
   # GET /brands
   # GET /brands.json
   def index
-    @brands = Brand.all
+    @brands = Brand.all.order('name ASC')
   end
 
   # GET /brands/1
